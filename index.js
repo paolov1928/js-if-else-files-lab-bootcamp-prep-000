4b0ee2ccf33a7cd3dfb080ec0e636f3ef5f9d622
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-18 20:51:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var height = 70
 if (name === "Susan") {
     console.log("The name is correct")
 }
+<<<<<<< HEAD
 else {console.log("Expected the name to be Susan")}
+=======
+else {
+>>>>>>> ec6c0ce0b5906a9e86b0f0eba5a99d559daabf21
 
 // Don't worry about this
 module.exports = { name, height
